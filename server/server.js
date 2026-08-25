168f37e1a653e21794acbf68966526be18040f1b
NUKUL LILHARE <128592344+nukul6446@users.noreply.github.com>
2026-08-25 10:36:10 +0530

    Update allowedOrigins to include CLIENT_URL

--- server/server.js
+++ server/server.js
@@ -52,7 +52,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:3000",
-   process.env.CLIENT_URL.trim()   // important!
+  process.env.CLIENT_URL,
 ].filter(Boolean);
 
 console.log("🌐 Allowed origins:", allowedOrigins);
